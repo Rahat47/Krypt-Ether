1ac9d9cd44e2a7de7f5dff69fb2a39067f83d67a
Rahat47 <djrayhan8@gmail.com>
2021-12-30 11:27:12 +0600

    welcome component done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,19 +2,48 @@ import { AiFillPayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 import { Loader } from '.';
+import { useState } from 'react';
 
 const commonStyles =
     'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
+const Input = ({ placeholder, type, name, value, handleChange }) => {
+    return (
+        <input
+            type={type}
+            name={name}
+            value={value}
+            onChange={handleChange}
+            placeholder={placeholder}
+            step={0.0001}
+            className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+        />
+    );
+};
+
 const Welcome = () => {
+    const [loading, setLoading] = useState(false);
+
     const connectWallet = () => {
         console.log('Not Implemented Yet!!!');
     };
 
+    const handleChange = e => {
+        console.log(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    };
+
     return (
         <div className='flex w-full justify-center items-center'>
-            <div className='flex md:flex-row items-start justify-between md:p-20 py-12 px-4'>
-                <div className='flex flex-1 justify-start flex-col md:mr-10'>
+            <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+                <div className='flex flex-1 justify-start flex-col mf:mr-10'>
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
                         Send Crypto <br /> across the world
                     </h1>
@@ -49,8 +78,73 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
-                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'></div>
+                <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
+                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+                        <div className='flex justify-between flex-col w-full h-full'>
+                            <div className='flex justify-between items-start'>
+                                <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                                    <SiEthereum fontSize={21} color='#fff' />
+                                </div>
+                                <BsInfoCircle fontSize={17} color='#fff' />
+                            </div>
+
+                            <div>
+                                <p className='text-white font-light text-sm'>
+                                    Address
+                                </p>
+
+                                <p className='text-white font-semibold text-lg mt-1'>
+                                    Ethereum
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='p-5 sm:w-96 w-full flex-col flex justify-start items-center blue-glassmorphism'>
+                        <Input
+                            placeholder={'Address To'}
+                            name={'addressTo'}
+                            type={'text'}
+                            handleChange={handleChange}
+                        />
+                        <Input
+                            placeholder={'Amount (ETH)'}
+                            name={'amount'}
+                            type={'number'}
+                            handleChange={handleChange}
+                        />
+                        <Input
+                            placeholder={'Keyword (Gif)'}
+                            name={'keyword'}
+                            type={'text'}
+                            handleChange={handleChange}
+                        />
+                        <Input
+                            placeholder={'Enter Message'}
+                            name={'message'}
+                            type={'text'}
+                            handleChange={handleChange}
+                        />
+
+                        <div className='h-[1px] w-full bg-gray-400 my-2' />
+
+                        {loading ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type='button'
+                                onClick={handleSubmit}
+                                className='flex flex-row justify-center items-center bg-[#2952e3] rounded-full cursor-pointer hover:bg-[#2546bd] transition-all text-white text-base font-semibold w-full mt-2 border p-2 border-[#3d4f7c]'
+                            >
+                                <AiFillPayCircle
+                                    className='mr-2'
+                                    fontSize={21}
+                                    color='#fff'
+                                />{' '}
+                                Send Now
+                            </button>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
